8705e40e38f3b3c94db0916102af639d7237e7e1
Maryam Noor Fatima <maryamnf03@gmail.com>
2025-02-19 22:47:51 +0500

    Fixed Vercel deployment settings

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "BLAHHHHHHHHHHHHHHHHHHHHHHHHH"
+    return "okkkkkkkkkkkkkk"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
